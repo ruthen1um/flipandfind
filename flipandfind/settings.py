dc7fbb58a22ccfc3d63fc6be1b12dea8db8241d3
Oleg Fedotov <ol.fed25@gmail.com>
2025-02-18 22:48:47 +0300

    move files

--- flipandfind/settings.py
+++ flipandfind/settings.py
@@ -116,9 +116,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = '/static'
 STATIC_URL = 'static/'
+
+
+# Media files
+
+MEDIA_ROOT = '/media'
+MEDIA_URL = 'media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
