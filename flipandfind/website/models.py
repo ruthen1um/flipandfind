4bf11c262bcfef1538ba774336c8caec80e48711
Oleg Fedotov <ol.fed25@gmail.com>
2025-02-18 22:18:22 +0300

    add models for products and product photos

--- flipandfind/website/models.py
+++ flipandfind/website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField(max_length=7)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class ProductPhoto(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,
+                                related_name='photos')
+    photo = models.ImageField(upload_to='product_photos/')
