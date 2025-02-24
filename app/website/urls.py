fc19bcbdbc56599ed652571e693cc33872be0903
Oleg Fedotov <ol.fed25@gmail.com>
2025-02-24 23:38:52 +0300

    add names to urls to reliably reference them in templates

--- app/website/urls.py
+++ app/website/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = "website"
 urlpatterns = [
-    path("", views.catalog),
-    path("login", views.login),
-    path("cart", views.cart),
-    path("orders", views.orders),
-    path("product/<int:id>", views.product),
-    path("profile", views.profile)
+    path("", views.catalog, name="catalog"),
+    path("login", views.login, name="login"),
+    path("cart", views.cart, name="cart"),
+    path("orders", views.orders, name="orders"),
+    path("product/<int:id>", views.product, name="product"),
+    path("profile", views.profile, name="profile")
 ]
