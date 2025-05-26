88d9983f69334f1445deb81ef86632a01303ad18
Oleg Fedotov <ol.fed25@gmail.com>
2025-05-26 23:57:59 +0300

    add registration functionality for sellers

--- app/store/urls/seller.py
+++ app/store/urls/seller.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", RedirectView.as_view(pattern_name='store_seller:dashboard', permanent=False)),
     path("start", seller.start, name="start"),
     path("login", seller.login, name="login"),
+    path("registration", seller.registration, name="registration"),
     path("logout", seller.logout, name="logout"),
     path("dashboard", seller.dashboard, name="dashboard"),
     path("reviews", seller.reviews, name="reviews"),
